5519453d31cda67c9875aacc9ff9aa8ae0709dc0
malcosll <162439457+malcosll@users.noreply.github.com>
2024-05-09 08:09:44 -0300

    Add files via upload

---
+++ Desafios/desafio5.js
@@ -0,0 +1,5 @@
+const nomes = ["João", "Ana", "Caio", "Laura", "Marjorie", "Leo"];
+
+nomes.splice(1,2, "Rodrigo")
+
+console.log(nomes)
---
+++ Desafios/desafio6.js
@@ -0,0 +1,8 @@
+const salaJS = ["Edvaldo", "Camila", "Beatriz"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const SalaUnificadas = salaJS.concat(salaPython);
+const SalaUnificadas2 = salaPython.concat(salaJS);
+
+console.log(SalaUnificadas)
+console.log(SalaUnificadas2)
